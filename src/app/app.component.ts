5b8d8b1e72836cd6e0caebb03751f6cb5ea7cebc
Hagane <roymustangtheflamealchemist@hotmail.com>
2018-09-16 22:33:10 -0500

    añadir instrucciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
+
+import moment from 'moment';
+
+moment.locale('es-MX');
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [
+
+  ],
 })
 export class AppComponent {
   title = 'prueba';
+
+  //Variables for date
+  date: FormControl;
+  today: Date;
+
+  //Variables to set min and max allowed dates
+  minDate: Date;
+  maxDate: Date;
+
+  //Variables to hold the selected user dates
+  startDate: Date;
+  endDate: Date;
+  dates;
+  dates2;
+
+  constructor() {
+    this.date = new FormControl(new Date());
+    this.today = new Date();
+    this.minDate = moment(this.today).subtract(2, 'months').toDate();
+    this.maxDate = this.today;
+  }
+
+  ngOnInit() {
+    
+  }
+
+  onDaySelected(event: MatDatepickerInputEvent<Date>) {
+    console.log(moment(event.value).format('L'));
+    this.dates = (moment(event.value).format('L'));
+  }
+
+  onWeekSelected(event: MatDatepickerInputEvent<Date>) {
+    const day = moment(event.value).weekday();
+    this.startDate = moment(event.value).subtract(day, 'days').toDate();
+    this.endDate = moment(event.value).add((6 - day), 'days').toDate();
+    console.log("startDate: " + moment(this.startDate).format('L'));
+    console.log("endDate: " + moment(this.endDate).format('L'));
+    this.dates2 = moment(this.startDate).format('L') + " - " + moment(this.endDate).format('L');
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDatepickerModule, MatFormFieldModule, MatInputModule, MatNativeDateModule } from '@angular/material';
+import { MatMomentDateModule, MAT_MOMENT_DATE_FORMATS, MomentDateAdapter } from '@angular/material-moment-adapter';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
 
 import { AppComponent } from './app.component';
+
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'L',
+  },
+  display: {
+    dateInput: 'L',
+    monthYearLabel: 'MMMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule,
+    MatMomentDateModule,
   ],
-  providers: [],
+  exports: [
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule,
+    MatMomentDateModule,
+  ],
+  providers: [
+    {provide: MAT_DATE_LOCALE, useValue: 'es-MX'},
+    {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
+    {provide: MAT_DATE_FORMATS, useValue: MY_FORMATS},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
